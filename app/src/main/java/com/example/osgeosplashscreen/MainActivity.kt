c6f1fda3fd6e7631cf69db783b10e96480c662cc
TanvirSingh007 <singhtanvir032@gmail.com>
2019-12-04 23:40:50 +0530

    first commit

---
+++ app/src/main/java/com/example/osgeosplashscreen/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.osgeosplashscreen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        setContentView(R.layout.activity_main)
+        Handler().postDelayed({
+
+            startActivity(Intent(this@MainActivity, FirstPage::class.java))
+
+            finish()
+        },3000)
+
+    }
+}
